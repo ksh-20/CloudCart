bd59fb2454e2650e89ab5a4cfce7a1d0962b19c6
Kshitij <kshitij24.srinivasan@gmail.com>
2025-10-27 23:37:51 +0530

    Modified Payment Integration

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,56 +1,33 @@
-const axios = require("axios")
+import Razorpay from "razorpay";
+import dotenv from "dotenv";
+dotenv.config();
 
-const CHAPA_URL = process.env.CHAPA_URL || "https://api.chapa.co/v1/transaction/initialize"
-const CHAPA_AUTH = process.env.CHAPA_AUTH // || register to chapa and get the key
+export const createOrder = async (req, res) => {
+  try {
+    // Initialize Razorpay instance
+    const instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
 
-const initializePayment = async (req, res) => {
+    // Order details
+    const options = {
+      amount: req.body.amount * 100, // amount in paise (1 INR = 100 paise)
+      currency: "INR",
+      receipt: `receipt_${Date.now()}`,
+    };
 
-    const config = {
-        headers: {
-            Authorization: CHAPA_AUTH
-        }
-    }
+    // Create order
+    const order = await instance.orders.create(options);
 
-    // chapa redirect you to this url when payment is successful
-    const CALLBACK_URL = "http://localhost:3000"
+    if (!order) return res.status(500).send("Order creation failed");
 
-    // a unique reference given to every transaction
-    const TEXT_REF = "tx-myecommerce12345-" + Date.now()
-
-    // form data
-    const data = {
-        amount: req.body.amount, 
-        currency: 'ETB',
-        email: 'ato@ekele.com',
-        first_name: 'Ato',
-        last_name: 'Ekele',
-        tx_ref: TEXT_REF,
-        callback_url: CALLBACK_URL
-    }
-
-    // post request to chapa
-    await axios.post(CHAPA_URL, data, config)
-        .then((response) => {
-            res.send(response.data.data.checkout_url)
-            console.log(response.data)
-        })
-        .catch((err) => console.log(err))
-
-        /* res.json({res: "message", url: CALLBACK_URL}) */
-}
-
-const verifyPayment = async (req, res) => {
-        await axios.get("https://api.chapa.co/v1/transaction/verify/" + req.params.id, config)
-            .then((response) => {
-                console.log(response)
-                res.json({message: response})
-            }) 
-            .catch((err) => {
-                console.log("Payment can't be verfied", err)
-                res.json({error: err})
-            })
-
-            res.json({message: "response", param: req.params.id})
-}
-
-module.exports = { initializePayment, verifyPayment }
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.error("Error creating Razorpay order:", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
